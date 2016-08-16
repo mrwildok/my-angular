a1232b31768932784bb7a5e6d28ceaf666d455dd
mrwildok <mrwkdesign@gmail.com>
2016-08-16 12:32:26 -0500

    programing

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,7 +3,10 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'main-danicargon',
     template: `
-    <a class="green">Button</a>
+    <a (click)="onSelect()" class="green">Button</a>
+    <div *ngIf="showMe === true">
+      <p>{{saludo.pregunta}}</p>
+    </div>
     <ul>
       <li *ngFor="#contacts of contacts">
         {{contacts.name}} {{contacts.lastname}} : {{contacts.mail}}
@@ -21,5 +24,11 @@ export class AppComponent {
   public contacts = [
     { name: "Daniel" , lastname: "Cárdenas" , mail: "daniel.cardenas@controlbox.net"} , { name: "Valentina" , lastname: "Salazar" , mail: "valentina.salzar@controlbox.net"} , { name: "manuela" , lastname: "Villada" , mail: "manuel.vilalda@controlbox.net"},
   ];
-  public saludo = {name: "your name here"};
+  public saludo = {name: "your name here" , pregunta: "Can you see me now?" };
+
+  public showMe = false;
+
+  onSelect() {
+    this.showMe = true;
+  }
 }
