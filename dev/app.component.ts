03832aaaa3e2db5932bdf712f5efbf650a3efbe1
mrwildok <mrwkdesign@gmail.com>
2016-08-16 12:21:06 -0500

    styles

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,11 +3,17 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'main-danicargon',
     template: `
+    <a class="green">Button</a>
     <ul>
       <li *ngFor="#contacts of contacts">
         {{contacts.name}} {{contacts.lastname}} : {{contacts.mail}}
       </li>
     </ul>
+    <label>
+      Find something
+      <input [(ngModel)]="saludo.name" type="text">
+    </label>
+    <h3>Hi my name is {{saludo.name}}</h3>
     `,
     styleUrls: ["../src/css/app.css"]
 })
@@ -15,4 +21,5 @@ export class AppComponent {
   public contacts = [
     { name: "Daniel" , lastname: "Cárdenas" , mail: "daniel.cardenas@controlbox.net"} , { name: "Valentina" , lastname: "Salazar" , mail: "valentina.salzar@controlbox.net"} , { name: "manuela" , lastname: "Villada" , mail: "manuel.vilalda@controlbox.net"},
   ];
+  public saludo = {name: "your name here"};
 }
